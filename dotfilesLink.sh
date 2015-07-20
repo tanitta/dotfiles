599065647476ab14471b644b4584a29a7ab40b8f
tanitta <ttata.trit@gmail.com>
2015-07-20 12:28:44 +0900

    add .Xresources

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.xmonad ~/.xmonad
 ln -s ~/dotfiles/.xmobarrc ~/.xmobarrc
+ln -s ~/dotfiles/.Xresources ~/.Xresources   
